713d4a1fd26d37c5371a8cd68cee50822741b5b0
Rahul Gupta <rahulgupta@gmail.com>
2022-12-05 21:05:36 +0530

    - Created a button for turning on all the relays - And some minor styling issue

--- components/control-page.tsx
+++ components/control-page.tsx
@@ -13,6 +13,7 @@ export default function ControlPage() {
   const [value2, setValue2] = useState(false);
   const [value3, setValue3] = useState(false);
   const [value4, setValue4] = useState(false);
+  const [turnOn, setTurnOn] = useState(false);
   const [turnOff, setTurnOff] = useState(false);
   get(child(refDb, `/Data`))
     .then((snapshot) => {
@@ -26,9 +27,14 @@ export default function ControlPage() {
       console.error(error);
     })
   useEffect(() => {
-    if (value1 || value2 || value3 || value4)
+    if (value1 || value2 || value3 || value4) {
       setTurnOff(false)
+      setTurnOn(false)
+    }
     else setTurnOff(true)
+    if (value1 && value2 && value3 && value4)
+      setTurnOn(true)
+    else setTurnOn(false)
   }, [value1, value2, value3, value4])
 
 
@@ -51,6 +57,20 @@ export default function ControlPage() {
         setValue4(false)
       })
   }
+  const turnOnAll = () => {
+    const updates: { [key: string]: boolean } = {}
+    updates['/Data/Relay1'] = true
+    updates['/Data/Relay2'] = true
+    updates['/Data/Relay3'] = true
+    updates['/Data/Relay4'] = true
+    return update(refDb, updates)
+      .then(() => {
+        setValue1(true)
+        setValue2(true)
+        setValue3(true)
+        setValue4(true)
+      })
+  }
 
   return (
     <>
@@ -64,6 +84,7 @@ export default function ControlPage() {
         <ToggleSwitch className={styles.btn} label="Relay 3" value={value3} onChange={() => toggleRelay('3', value3).then(() => setValue3(!value3))} />
         <ToggleSwitch className={styles.btn} label="Relay 4" value={value4} onChange={() => toggleRelay('4', value4).then(() => setValue4(!value4))} />
         <Button variant='contained' className={styles.btn_last} onClick={turnOffAll} disabled={turnOff}>Turn off all appliances</Button>
+        <Button variant='contained' className={styles.btn_last} onClick={turnOnAll} disabled={turnOn}>Turn on all appliances</Button>
       </div>
     </>
   )
